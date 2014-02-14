59659bb59698bf7903da86174b7792285e27c4f4
Janni.Rogge@gmail.com <Janni.Rogge@gmail.com@46dfd002-a3e8-36c0-585c-09f87d1aaff5>
2014-02-14 21:09:41 +0000

    Aufbau und erste Ideen

---
+++ trunk/Sudoku/Sudoku/de/szut/sudoku/console/Console.java
@@ -0,0 +1,19 @@
+package de.szut.sudoku.console;
+
+import de.szut.sudoku.game.IUI;
+
+public class Console implements IUI{
+
+	@Override
+	public void showField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/Sudoku/Sudoku/de/szut/sudoku/game/Main.java
@@ -0,0 +1,10 @@
+package de.szut.sudoku.game;
+
+import de.szut.sudoku.jGui.GameFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame();
+		
+	}
+}
