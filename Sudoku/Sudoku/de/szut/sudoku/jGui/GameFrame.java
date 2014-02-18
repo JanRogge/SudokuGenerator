de9b6339fb6886bdcffa946d2d22beca6054c472
Janni.Rogge@gmail.com <Janni.Rogge@gmail.com@46dfd002-a3e8-36c0-585c-09f87d1aaff5>
2014-02-18 09:51:08 +0000

    Lable Listener

--- Sudoku/Sudoku/de/szut/sudoku/jGui/GameFrame.java
+++ Sudoku/Sudoku/de/szut/sudoku/jGui/GameFrame.java
@@ -2,14 +2,29 @@ package de.szut.sudoku.jGui;
 
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame {
+import de.szut.sudoku.game.IUI;
+
+public class GameFrame extends JFrame implements IUI{
 	public GameFrame() {
+		setTitle("Sudoku");
 		setVisible(true);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 500);
+		setBounds(100, 100, 445, 500);
 		setLayout(null);
 		setContentPane(new GameField());
+	}
+
+	@Override
+	public void showField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendData() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Sudoku/Sudoku/de/szut/sudoku/jGui/Listener.java
+++ Sudoku/Sudoku/de/szut/sudoku/jGui/Listener.java
@@ -6,11 +6,23 @@ import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
 
 public class Listener extends MouseAdapter{
+	private JLabel oldLable;
+	private JLabel newLable;
 	public void mouseClicked(MouseEvent clk) {
-		JLabel test = (JLabel) clk.getSource();
-		test.requestFocusInWindow();
-		if (test.requestFocusInWindow()){
-			test.setBackground(Color.gray);
-		}
+		newLable = (JLabel) clk.getSource();
+		newLable.requestFocusInWindow();
+		if (newLable.isEnabled() != false){
+			if (newLable == oldLable){
+				newLable.setBackground(Color.gray);
+			}
+			else if (oldLable != null){
+				newLable.setBackground(Color.gray);
+				oldLable.setBackground(Color.white);
+			}
+			else{
+				newLable.setBackground(Color.white);
+			}
+			oldLable = newLable;
+		}		
 	}
 }
